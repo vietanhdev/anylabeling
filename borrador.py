0a4293bd3c91a2148bcd6e627e386e5f32275147
hdnh2006 <henry.matucv@gmail.com>
2023-04-13 18:42:58 +0200

    borrador updated

--- borrador.py
+++ borrador.py
@@ -7,20 +7,32 @@ Created on Thu Apr 13 10:16:59 2023
 """
 
 import cv2
-net = cv2.dnn.readNet("/home/henry/anylabeling_data/models/yolov5/yolov8x.onnx")
-net.setPreferableBackend(cv2.dnn.DNN_BACKEND_DEFAULT)
-net.setPreferableTarget(cv2.dnn.DNN_TARGET_CPU)
-input_image = cv2.imread("coco_sample/000000000009.jpg")
+from PIL import Image
+import numpy as np
+net = cv2.dnn.readNetFromONNX("/home/henry/anylabeling_data/models/yolov5/yolov8x.onnx")
+#net.setPreferableBackend(cv2.dnn.DNN_BACKEND_CUDA)
+#net.setPreferableTarget(cv2.dnn.DNN_TARGET_CUDA)
 
-blob = cv2.dnn.blobFromImage(input_image, 1 / 255, (640, 640), swapRB=True, crop=False)
+
+
+
+config = {'type': 'yolov8', 'name': 'yolov8x', 'display_name': 'YOLOv5n Ultralytics', 'model_path': 'anylabeling_assets/models/yolov5/yolov8x.onnx', 'input_width': 640, 'input_height': 640, 'score_threshold': 0.5, 'nms_threshold': 0.45, 'confidence_threshold': 0.45, 'classes': ['person', 'bicycle', 'car', 'motorcycle', 'airplane', 'bus', 'train', 'truck', 'boat', 'traffic light', 'fire hydrant', 'stop sign', 'parking meter', 'bench', 'bird', 'cat', 'dog', 'horse', 'sheep', 'cow', 'elephant', 'bear', 'zebra', 'giraffe', 'backpack', 'umbrella', 'handbag', 'tie', 'suitcase', 'frisbee', 'skis', 'snowboard', 'sports ball', 'kite', 'baseball bat', 'baseball glove', 'skateboard', 'surfboard', 'tennis racket', 'bottle', 'wine glass', 'cup', 'fork', 'knife', 'spoon', 'bowl', 'banana', 'apple', 'sandwich', 'orange', 'broccoli', 'carrot', 'hot dog', 'pizza', 'donut', 'cake', 'chair', 'couch', 'potted plant', 'bed', 'dining table', 'toilet', 'tv', 'laptop', 'mouse', 'remote', 'keyboard', 'cell phone', 'microwave', 'oven', 'toaster', 'sink', 'refrigerator', 'book', 'clock', 'vase', 'scissors', 'teddy bear', 'hair drier', 'toothbrush']}
+classes = config["classes"]
+
+
+
+input_image = cv2.imread('bus.jpg')
+#input_image = cv2.imread('coco_sample/000000000009.jpg')
+[height, width, _] = input_image.shape
+length = max((height, width))
+image = np.zeros((length, length, 3), np.uint8)
+image[0:height, 0:width] = input_image
+
+blob = cv2.dnn.blobFromImage(image, scalefactor=1 / 255, size=(640, 640), swapRB=True)
 
 net.setInput(blob)
-
-
-# Runs the forward pass to get output of the output layers.
-output_layers = net.getUnconnectedOutLayersNames()
 outputs = net.forward()
-outputs = outputs.transpose((0, 2, 1))
+outputs = np.array([cv2.transpose(outputs[0])])
 
 
 
@@ -35,7 +47,7 @@ confidences = []
 boxes = []
 
 # Rows.
-rows = outputs[0].shape[0]
+rows = outputs[0].shape[1]
 
 image_height, image_width = input_image.shape[:2]
 
@@ -49,14 +61,14 @@ for r in range(rows):
     confidence = row[4]
 
     # Discard bad detections and continue.
-    if confidence >= config["confidence_threshold"]:
-        classes_scores = row[5:]
+    if confidence >= self.config["confidence_threshold"]:
+        classes_scores = row[4:]
 
         # Get the index of max class score.
         class_id = np.argmax(classes_scores)
 
         #  Continue if the class score is above threshold.
-        if classes_scores[class_id] > self.config["score_threshold"]:
+        if classes_scores[class_id] > config["score_threshold"]:
             confidences.append(confidence)
             class_ids.append(class_id)
 
@@ -75,7 +87,8 @@ for r in range(rows):
 indices = cv2.dnn.NMSBoxes(
     boxes,
     confidences,
-    config["confidence_threshold"],
+    0.001,
+    #config["confidence_threshold"],
     config["nms_threshold"],
 )
 
@@ -99,3 +112,7 @@ for i in indices:
     }
 
     output_boxes.append(output_box)
+    cv2.rectangle(input_image, (left, top), (left + width, top + height), (0,255,0), 3)
+
+cv2.imwrite("test.jpg", input_image)
+#Image.fromarray(cv2.cvtColor(input_image, cv2.COLOR_BGR2RGB))
