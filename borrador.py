4cd7e8164ff43ce6f9ae6c12ab9644c304bd6cab
hdnh2006 <henry.matucv@gmail.com>
2023-04-19 14:39:01 +0200

    segmentation works

--- borrador.py
+++ borrador.py
@@ -4,29 +4,6 @@ import numpy.typing as npt
 from typing import Tuple, Iterable
 
 
-def handle_overflow_box(
-    box: npt.NDArray[np.int32], imgsz: Tuple[int, int]
-) -> npt.NDArray[np.int32]:
-    """Handle if box contain overflowing coordinate based on image size
-    Args:
-        box (npt.NDArray[np.int32]): box to draw [left, top, width, height]
-        imgsz (Tuple[int, int]): Current image size [width, height]
-    Returns:
-        Non overflowing box
-    """
-    if box[0] < 0:
-        box[0] = 0
-    elif box[0] >= imgsz[0]:
-        box[0] = imgsz[0] - 1
-    if box[1] < 0:
-        box[1] = 0
-    elif box[1] >= imgsz[1]:
-        box[1] = imgsz[1] - 1
-    box[2] = box[2] if box[0] + box[2] <= imgsz[0] else imgsz[0] - box[0]
-    box[3] = box[3] if box[1] + box[3] <= imgsz[1] else box[3] - box[1]
-    return box
-
-
 
 
 
@@ -48,6 +25,7 @@ classes = config["classes"]
 
 # Load input image
 image = cv2.imread('bus.jpg')
+source_padded = image.copy()
 overlay = image.copy()  # make overlay mat
 
 # Preprocess image
@@ -70,6 +48,7 @@ boxes = []
 # Rows.
 rows = outputs[0].shape[1]
 h, w = image.shape[:2]
+h_source, w_source = image.shape[:2]
 # Resizing factor.
 x_factor = w / 640
 y_factor =  h / 640
@@ -95,6 +74,7 @@ max_size = max(h,w)
 _, seg_chanels, seg_width, seg_height = outputs[1].shape
 colors= [[0,255,0]]*len(classes)
 selected_boxes = []
+my_masks=[]
 for i in selected:  # loop through selected
     box = boxes[i].round().astype(np.int32)  # to int
     #box = handle_overflow_box(box, [max_size, max_size])  # handle overflow boxes
@@ -108,10 +88,16 @@ for i in selected:  # loop through selected
         boxes_to_draw.append([box, classes[label[1]], score, color])
 
         # crop mask from proto
-        x = int(round(box[0] ))
-        y = int(round(box[1]  ))
-        w = int(round(box[2]  ))
-        h = int(round(box[3] ))
+        # x = int(round(box[0] ))
+        # y = int(round(box[1]  ))
+        # w = int(round(box[2]  ))
+        # h = int(round(box[3] ))
+        
+        # crop mask from proto
+        x = int(round(box[0] * 160/ max_size))
+        y = int(round(box[1] * 160 / max_size))
+        w = int(round(box[2] * 160 / max_size))
+        h = int(round(box[3] * 160 / max_size))
 
 
         # process protos
@@ -119,7 +105,7 @@ for i in selected:  # loop through selected
         protos = np.expand_dims(masks[i], 0) @ protos  # matmul
         protos = 1 / (1 + np.exp(-protos))  # sigmoid
         protos = protos.reshape(h, w)  # reshape
-        mask = cv2.resize(protos, (box[2], box[3]))  # resize mask
+        mask = cv2.resize(protos, (w_source, h_source))  # resize mask
         mask = mask >= 0.5 #config["score_threshold"] # mask_tresh  # filtering mask by tresh
 
         # add mask to overlay layer
@@ -133,8 +119,9 @@ for i in selected:  # loop through selected
 
 boxes = selected_boxes
 
-
-
+# combine image and overlay
+source_padded = cv2.addWeighted(source_padded, 1 - 0.5, overlay, 0.5, 0)
+cv2.imwrite("test.jpg", overlay)
 
 for box in selected_boxes:
     x = box[0]
@@ -160,108 +147,3 @@ for box in selected_boxes:
 cv2.imwrite("test.jpg", image)
 
 
-
-
-
-
-
-
-
-
-
-
-
-"""
-
-
-
-
-for r in range(rows):
-    
-    row = outputs[0][0][r]
-    
-
-    classes_scores = row[5:len(config["classes"]) + 5]
-    # Get the index of max class score and confidence.
-    _, confidence, _, (_, class_id) = cv2.minMaxLoc(classes_scores)
-    
-        
-    if (confidence > config["confidence_threshold"]):
-    
-        cx, cy, w, h = row[0], row[1], row[2], row[3]
-
-        left = int((cx - w / 2) * x_factor)
-        top = int((cy - h / 2) * y_factor)
-        width = int(w * x_factor)
-        height = int(h * y_factor)
-        box = np.array([left, top, width, height])
-        mask = row[len(config["classes"]) + 5 :]
-        
-        class_ids.append(class_id)
-        confidences.append(confidence)
-        segmentation_masks.append(mask)
-        boxes.append(box)
-        
-
-
-indices = cv2.dnn.NMSBoxes(
-    boxes,
-    confidences,
-    config["confidence_threshold"],
-    config["nms_threshold"],
-)
-
-
-
-output_boxes = []
-output_masks = []
-
-
-for i in indices:
-    mask = segmentation_masks[i]
-    box = boxes[i]
-    left = int(box[0])
-    top = int(box[1])
-    width = int(box[2])
-    height = int(box[3])
-    label = classes[class_ids[i]]
-    score = confidences[i]
-
-    output_box = {
-        "x1": left,
-        "y1": top,
-        "x2": left + width,
-        "y2": top + height,
-        "label": label,
-        "score": score,
-    }
-
-    output_boxes.append(output_box)
-    output_masks.append(mask)
-    cv2.rectangle(image, (left, top), ((left + width), (top + height)), (0,255,0), 3)
-
-cv2.imwrite("test.jpg", image)
-
-
-
-
-
-
-# Draw bounding boxes and apply masks on the image
-for i in range(len(class_ids)):
-    class_id = class_ids[i]
-    confidence = confidences[i]
-    mask = segmentation_masks[i]
-    color = (0, 255, 0)  # set color for bounding box here
-    label = f"Class {class_id}, Confidence: {confidence:.2f}"
-    cv2.rectangle(image, (left, top), (left + width, top + height), color, 2)
-    cv2.putText(image, label, (left, top - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.5, color, 2)
-    image = cv2.bitwise_and(image, image, mask=mask)
-
-# Show the final result
-cv2.imwrite("test.jpg")
-# cv2.imshow("YOLOv5 Segmentation", image)
-# cv2.waitKey(0)
-# cv2.destroyAllWindows()
-
-"""
