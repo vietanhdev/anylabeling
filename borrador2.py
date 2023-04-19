a073958493fa008a96fd156a09a5c22ab605f6ea
hdnh2006 <henry.matucv@gmail.com>
2023-04-19 16:30:58 +0200

    finally I could correctly put segmentations

--- borrador2.py
+++ borrador2.py
@@ -3,25 +3,13 @@ from typing import Union
 import cv2
 import numpy as np
 import numpy.typing as npt
+from typing import Tuple, Iterable
 
-def get_divable_size(imgsz: Iterable[int], stride: int) -> Iterable[int]:
-    """Get divable image size by model stride
-    Args:
-        imgsz (Iterable[int]): Current image size [width, height]
-        stride (int): Model stride
-    Returns:
-        Divable image size by model stride
-    """
-    for i in range(len(imgsz)):
-        div, mod = divmod(imgsz[i], stride)
-        if mod > stride / 2:
-            div += 1
-        imgsz[i] = div * stride
-    return imgsz
+
 
 
 labels = ['person', 'bicycle', 'car', 'motorcycle', 'airplane', 'bus', 'train', 'truck', 'boat', 'traffic light', 'fire hydrant', 'stop sign', 'parking meter', 'bench', 'bird', 'cat', 'dog', 'horse', 'sheep', 'cow', 'elephant', 'bear', 'zebra', 'giraffe', 'backpack', 'umbrella', 'handbag', 'tie', 'suitcase', 'frisbee', 'skis', 'snowboard', 'sports ball', 'kite', 'baseball bat', 'baseball glove', 'skateboard', 'surfboard', 'tennis racket', 'bottle', 'wine glass', 'cup', 'fork', 'knife', 'spoon', 'bowl', 'banana', 'apple', 'sandwich', 'orange', 'broccoli', 'carrot', 'hot dog', 'pizza', 'donut', 'cake', 'chair', 'couch', 'potted plant', 'bed', 'dining table', 'toilet', 'tv', 'laptop', 'mouse', 'remote', 'keyboard', 'cell phone', 'microwave', 'oven', 'toaster', 'sink', 'refrigerator', 'book', 'clock', 'vase', 'scissors', 'teddy bear', 'hair drier', 'toothbrush']
-
+colors = np.random.randint(0, 256, size=(len(labels), 3)).tolist()
 
 model = cv2.dnn.readNet("/home/henry/anylabeling_data/models/yolov5/yolov5x-seg.onnx")
 
@@ -29,26 +17,24 @@ source = cv2.imread('bus.jpg')
 
 source_height, source_width, _ = source.shape
 
-## resize to divable size by stride
-source_width, source_height = get_divable_size([source_width, source_height], 32)
-source = cv2.resize(source, [source_width, source_height])
 
 ## padding image
 max_size = max(source_width, source_height)  # get max size
-source_padded = np.zeros((max_size, max_size, 3), dtype=np.uint8)  # initial zeros mat
-source_padded[:source_height, :source_width] = source.copy()  # place original image
+#source_padded = np.zeros((max_size, max_size, 3), dtype=np.uint8)  # initial zeros mat
+#source_padded[:source_height, :source_width] = source.copy()  # place original image
+source_padded= source.copy()  # place original image
 overlay = source_padded.copy()  # make overlay mat
 
 ## ratios
-x_ratio = max_size / 640
-y_ratio = max_size / 640
+x_ratio = source_width / 640
+y_ratio = source_height / 640
 
 # run model
 input_img = cv2.dnn.blobFromImage(
     source_padded,
     1 / 255.0,
     (640, 640),
-    swapRB=False,
+    swapRB=True,
     crop=False,
 )  # normalize and resize: [h, w, 3] => [1, 3, h, w]
 
@@ -78,12 +64,9 @@ masks = result[0][0, :, len(labels) + 5 :]
 selected = cv2.dnn.NMSBoxes(boxes, confidences, 0.45, 0.45, top_k=100)
 
 boxes_to_draw = []  # boxes to draw
-colors = [[0,255,0]]*80
 
 for i in selected:  # loop through selected
     box = boxes[i].round().astype(np.int32)  # to int
-    #box = handle_overflow_box(box, [max_size, max_size])  # handle overflow boxes
-
     _, score, _, label = cv2.minMaxLoc(scores[i])  # get score and classId
     print(label)
     if score >= 0.40:  # filtering by score_tresh
@@ -93,31 +76,49 @@ for i in selected:  # loop through selected
         boxes_to_draw.append([box, labels[label[1]], score, color])
 
         # crop mask from proto
-        x = int(round(box[0] * 160 / max_size))
-        y = int(round(box[1] * 160 / max_size))
-        w = int(round(box[2] * 160 / max_size))
-        h = int(round(box[3] * 160 / max_size))
+        # x = int(round(box[0] * 160 / max_size))
+        # y = int(round(box[1] * 160 / max_size))
+        # w = int(round(box[2] * 160 / max_size))
+        # h = int(round(box[3] * 160 / max_size))
+        
+        x = int(round(box[0] * 160 / source_width))
+        y = int(round(box[1] * 160 / source_height))
+        w = int(round(box[2] * 160 / source_width))
+        h = int(round(box[3] * 160 / source_height))
 
         # process protos
         protos = result[1][0, :, y : y + h, x : x + w].reshape(seg_chanels, -1)
+        print(x,x+w)
         protos = np.expand_dims(masks[i], 0) @ protos  # matmul
         protos = 1 / (1 + np.exp(-protos))  # sigmoid
         protos = protos.reshape(h, w)  # reshape
         mask = cv2.resize(protos, (box[2], box[3]))  # resize mask
         mask = mask >= 0.45  # filtering mask by tresh
 
-        # add mask to overlay layer
+        # # add mask to overlay layer
         to_mask = overlay[box[1] : box[1] + box[3], box[0] : box[0] + box[2]]  # get box roi
         mask = mask[: to_mask.shape[0], : to_mask.shape[1]]  # crop mask
         to_mask[mask] = color  # apply mask
 
 # combine image and overlay
+overlay[1] = overlay[1]+10000
 source_padded = cv2.addWeighted(source_padded, 1 - 0.4, overlay, 0.4, 0)
 
-# for draw_box in boxes_to_draw:  # draw boxes
-#     draw_boxes(source_padded, *draw_box)
 
 source = source_padded[:source_height, :source_width]  # crop padding
 
 cv2.imwrite("test.jpg",source)
 
+"""
+
+for box in boxes_to_draw:
+    box = box[0]
+    left = box[0]
+    top = box[1]
+    width = box[2]
+    height = box[3]
+
+    cv2.rectangle(source, (left, top), ((left + width), (top + height)), (0,255,0), 3)
+
+cv2.imwrite("test.jpg", source)
+"""
