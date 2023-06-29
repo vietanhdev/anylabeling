d5502d1f86dc1fc6a1d043641589a2f8f7af9880
Viet Anh Nguyen <vietanh.dev@gmail.com>
2023-06-29 08:13:35 +0700

    Bump to v0.3.2

--- anylabeling/app_info.py
+++ anylabeling/app_info.py
@@ -1,4 +1,4 @@
 __appname__ = "AnyLabeling"
 __appdescription__ = "Effortless data labeling with AI support"
-__version__ = "0.3.0"
+__version__ = "0.3.2"
 __preferred_device__ = "CPU"  # GPU or CPU
