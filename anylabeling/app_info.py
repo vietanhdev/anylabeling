a6a388bd21faedc0f084b68aa657e489e57a81a5
Viet-Anh Nguyen <vietanh.dev@gmail.com>
2024-08-03 14:45:10 +0700

    Disable macOS app build

--- anylabeling/app_info.py
+++ anylabeling/app_info.py
@@ -1,4 +1,4 @@
 __appname__ = "AnyLabeling"
 __appdescription__ = "Effortless data labeling with AI support"
-__version__ = "0.4.6"
+__version__ = "0.4.8"
 __preferred_device__ = "CPU"  # GPU or CPU
