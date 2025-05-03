c73e9fd0f99975f1eba5a10fc50ca93997b3da43
Viet-Anh Nguyen <vietanh.dev@gmail.com>
2025-05-03 23:48:15 +0700

    Fix build

--- anylabeling/app_info.py
+++ anylabeling/app_info.py
@@ -1,4 +1,4 @@
 __appname__ = "AnyLabeling"
 __appdescription__ = "Effortless data labeling with AI support"
-__version__ = "0.4.17"
+__version__ = "0.4.18"
 __preferred_device__ = "CPU"  # GPU or CPU
