fdfd6edeac07efa8266800038b886e20b7bf5a61
Viet-Anh NGUYEN <vietanh.dev@gmail.com>
2026-08-30 10:47:11 +0700

    chore: bump version to 0.4.43 (#270)

--- anylabeling/app_info.py
+++ anylabeling/app_info.py
@@ -1,4 +1,4 @@
 __appname__ = "AnyLabeling"
 __appdescription__ = "Effortless data labeling with AI support"
-__version__ = "0.4.42"
+__version__ = "0.4.43"
 __preferred_device__ = "CPU"  # GPU or CPU"
